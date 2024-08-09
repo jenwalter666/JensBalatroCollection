2456b260b5aec9dc735d5fe163d7143125f77870
stupxd <uknownvector@gmail.com>
2024-08-09 03:19:20 +0300

    uh how did this appear here

--- Incantation/default.lua
+++
@@ -1,8 +0,0 @@
-return {
-    misc = {
-        dictionary = {
-            incant_vanilla_mode = "Stack negative consumables only",
-            incant_instant_merge = "Stack instantly",
-        }
-    }
-}
