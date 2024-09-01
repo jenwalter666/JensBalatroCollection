513c2b65616e443bbfe31d7079c0726825626f40
jenwalter666 <60117721+jenwalter666@users.noreply.github.com>
2024-09-02 00:20:10 +0100

    raise aurinko's priority (addressing cryptid)

--- Aurinko/Aurinko.lua
+++ Aurinko/Aurinko.lua
@@ -4,7 +4,7 @@
 --- MOD_ID: aurinko
 --- MOD_AUTHOR: [jenwalter666]
 --- MOD_DESCRIPTION: Lets planets naturally appear with editions, applies editions to hands when leveling
---- PRIORITY: 98999999999
+--- PRIORITY: 9899999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
 --- BADGE_COLOR: 009cff
 --- PREFIX: aurinko
 --- VERSION: 0.4.3
@@ -403,6 +403,7 @@ function create_card(_type, area, legendary, _rarity, skip_materialize, soulable
 		func = function()
 			if not card.edition and (((_type == 'Planet' or _type == 'Planet_dx') and (obj.aurinko or (card.ability.consumeable and card.ability.consumeable.hand_type))) or AurinkoWhitelist[card.ability.name]) then
 				local edition = poll_edition('edi'..(key_append or '')..tostring(G.GAME.round_resets.ante), math.max(1, math.min(1 + ((G.GAME.round_resets.ante / 2) - 0.5), 10)), true)
+				print(tostring(edition))
 				if edition and not edition.aurinko_blacklist then
 					card:set_edition(edition)
 				end
@@ -412,3 +413,4 @@ function create_card(_type, area, legendary, _rarity, skip_materialize, soulable
 	}))
 	return card
 end
+
